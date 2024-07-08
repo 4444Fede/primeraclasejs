801d2bd3d3da2bfb2cf83f34358dac318d8ff695
4444Fede <federico.vega@gm2dev.com>
2024-07-08 11:47:08 -0300

    feat(script): finish console exercises

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+// NIVEL 1
+
+console.log("Hola, mundo!")
+console.log("Federico")
+const myObj = {pais:"Argentina", ciudad:"CABA"};
+console.info(myObj);
+
+// NIVEL 2
+
+console.log("Bienvenido al sistema")
+console.info("Cargando aplicacion...")
+console.warn("Advertencia: Espacio en disco bajo")
+console.error("Error: No se pudo conectar al servidor")
+
+// NIVEL 3
+
+console.log("  *  ")
+console.log(" * * ")
+console.log("* * *")
+
+
+// NIVEL 4
+
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+debugger;
+console.log("6")
+console.log("7")
+console.log("8")
+console.log("9")
+console.log("10")
