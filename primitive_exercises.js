3a1530d9fc82912ac6fdc3a3e3d14bb21c86dea5
4444Fede <federico.vega@gm2dev.com>
2024-07-08 12:57:34 -0300

    feat(primitive.js): add boolean type of line

--- primitive_exercises.js
+++ primitive_exercises.js
@@ -9,13 +9,18 @@ console.log("string, number, BigInt, boolean, undefined and null")
 
 x = "pijurria"
 console.log(typeof x)
+
 x = 4444
 console.log(typeof x)
+
 x = 444444442131231232144123132131231231231231424213123213124n
 console.log(typeof x)
-x = Boolean(expression)
+
+x = true
 console.log(typeof x)
+
 x = undefined
 console.log(typeof x)
+
 x = null
 console.log(typeof x)
