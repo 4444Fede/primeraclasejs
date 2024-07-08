b58b7a41958c94f1d5d6f44820b0f9b0b44d3b2b
4444Fede <federico.vega@gm2dev.com>
2024-07-08 12:55:14 -0300

    feat: add exercise 1 and 2

---
+++ primitive_exercises.js
@@ -0,0 +1,21 @@
+
+// EJERCICIO 1
+
+console.log("debugger, console, delete, do, for, if, while and var")
+
+// EJERCICIO 2
+
+console.log("string, number, BigInt, boolean, undefined and null")
+
+x = "pijurria"
+console.log(typeof x)
+x = 4444
+console.log(typeof x)
+x = 444444442131231232144123132131231231231231424213123213124n
+console.log(typeof x)
+x = Boolean(expression)
+console.log(typeof x)
+x = undefined
+console.log(typeof x)
+x = null
+console.log(typeof x)
