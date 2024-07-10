da051e792c10e5a72439ab2fb47a4ed419bf3d57
4444Fede <federico.vega@gm2dev.com>
2024-07-10 12:54:21 -0300

    feat(variables): add exercise 1 to 9 code

---
+++ variables.js
@@ -0,0 +1,99 @@
+
+// EJERCICIO 1
+
+let username = 'Pijurria'
+
+// EJERCICIO 2
+
+let pi = 3.14159
+
+// EJERCICIO 3
+
+let itsYourBirthday = true
+
+// EJERCICIO 4
+
+let name = 'Agustin'
+let lastName = 'Iturria'
+let fullName = name + lastName
+
+// EJERCICIO 5
+
+let number1 = 4
+let number2 = 40
+
+	let addition = number1 + number2
+	let subtraction = number1 - number2
+	let multiplication = number1 * number2
+	let division = number1 / number2
+	let remainder = number1 % number2
+
+// EJERCICIO 6
+
+let number
+let phrase
+	
+	let concatenate_chain = phrase + number // Esto devuelve un string
+
+// EJERCICIO 7
+
+let online = true
+let away = !online
+
+// EJERCICIO 8
+
+let numberExercise8 = 4444;
+
+	numberExercise8++;
+	numberExercise8 += 1;
+	numberExercise8--;
+	numberExercise8 -= 1;
+	numberExercise8 = numberExercise8 * 2;
+	numberExercise8 *= 2;
+	numberExercise8 = numberExercise8 / 2;
+	numberExercise8 /= 2;
+
+// EJERCICIO 9
+
+const taxes = 20
+taxes = 10  // OUTPUT: TypeError: Assignment to constant variable.
+
+// EJERCICIO 10
+
+let firstNumber = 37
+let secondNumber = 73
+let firstPhrase = 'enano anabolico'
+let secondPhrase = 'Lionel Messi el GOAT'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
