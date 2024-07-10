8da0768fec089c6d4d442d4975781d0bff45fb67
4444Fede <federico.vega@gm2dev.com>
2024-07-10 10:25:54 -0300

    feat(exercise4): add code for exercise 4

---
+++ operators.js
@@ -0,0 +1,39 @@
+
+// EJERCICIO 1
+
+console.log(15 + 30);
+console.log(100 - 45);
+console.log(8 * 7);
+console.log(56 / 8);
+console.log(27 % 4);
+
+// EJERCICIO 2
+
+console.log(20 + 22);
+console.log("Hola" + "Mundo");
+console.log(5 * (3 + 2));
+
+// EJERCICIO 3
+
+console.log("El número es: " + 4);
+console.log("El número es: " + 4 + " y es sublime");
+console.log("El resultado de 2 + 2 es: " + (2 + 2));
+
+// EJERCICIO 4
+
+console.log(4 == "4"); 
+console.log(4 === 4); 
+console.log(4 == "8"); 
+console.log(4 === 2); 
+console.log(4 != 10); 
+console.log(4 !== "4"); 
+console.log(10 != "10"); 
+console.log(10 !== 10); 
+console.log(4 > 2); 
+console.log(4 < 44); 
+console.log(4 >= 4); 
+console.log(44 <= 4444); 
+console.log(2 > 4); 
+console.log(44 < 4); 
+console.log(4 >= 44); 
+console.log(4444 <= 44); 
